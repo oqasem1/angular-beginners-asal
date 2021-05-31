a407cc4cbc5e663381694b12052d38a0868c5cb4
oqasem <oqasem@apple.com>
2021-05-31 11:01:33 +0300

    Fix getValues for the login form as the submit event of the form

--- 04-angular-basics/login-first-app/src/auth/login/login.component.ts
+++ 04-angular-basics/login-first-app/src/auth/login/login.component.ts
@@ -10,9 +10,9 @@ import { LoginService } from '../login.service';
 export class LoginComponent implements OnInit {
 
   email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
 
-
-  password = '';
+  //password = '';
   hide = true;
 
   tokenObj: any =[];
@@ -26,30 +26,44 @@ export class LoginComponent implements OnInit {
     console.log("email: " + (event.target.value))
   }
   userTypedPassword(event: any) {
-    console.log("password: " + (this.password = event.target.value))
+    console.log("password: " + (event.target.value))
   }
-  getValues(event: any, email: any, password: any) {
+  getValues() {
 
-    console.log("Submitted email: " + email.value)
-    console.log("Submitted Password: " + password)
-
-    // Call https://academeez-login-ex.herokuapp.com/api/users/login through loginService
-
-    this._loginService.sendPostRequest(email.value, password).subscribe((token)=>{
-      this.tokenObj = token;
-       // then print the token object to the console
-      console.log("Token: "+this.tokenObj.token)
-    })
-
+    console.log("Submitted email: " + this.email.value)
+    console.log("Submitted Password: " + this.password.value)
 
   }
 
-  getErrorMessage() {
+  getEmailErrorMessage() {
     if (this.email.hasError('required')) {
-      return 'You must enter a value';
+      return 'You must enter an email';
     }
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a password';
+    }
+    return '';
+  }
+
+
+  onSubmit() {
+
+    this.getValues()
+    console.log('Form submitted!');
+
+        // Call https://academeez-login-ex.herokuapp.com/api/users/login through loginService
+
+        this._loginService.sendPostRequest(this.email.value, this.password.value).subscribe((token)=>{
+          this.tokenObj = token;
+           // then print the token object to the console
+          console.log("Token: "+this.tokenObj.token)
+        })
+
   }
 
 
